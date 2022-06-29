294284fb97a4f35f8fbcc36e78f36eb53e665a37
tarun <tarun.nagasai.92@gmail.com>
2022-06-29 23:11:44 +0530

    Initial

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 window=Tk()
 window.geometry("300x300")
 window.title("Road accident prediction model")
@@ -15,6 +16,38 @@ s = 160
 for i,j in values1.items():
     Radiobutton(window,text=i,value = j).place(x=40,y = s+20)
     s+=20
+
+n = StringVar()
+label3 = Label(window,text='Day of the week').place(x=40,y=240)
+monthchoosen = ttk.Combobox(window, width=27, textvariable=n)
+
+monthchoosen['values'] = (' Sunday',
+                          ' Monday',
+                          ' Tuesday',
+                          ' Wednsday',
+                          ' Thursday',
+                          ' Friday',
+                          ' Saturday')
+monthchoosen.place(x=40, y=260)
+monthchoosen.current(1)
+
+n = StringVar()
+label4 = Label(window,text='Special conditions at site').place(x=40,y=300)
+conditionchoosen = ttk.Combobox(window, width=27, textvariable=n)
+
+conditionchoosen['values'] =(' None',
+                          ' Roadworks',
+                          ' Oil or diesel',
+                          ' Mud',
+                          ' Road surface defective',
+                          ' Auto traffic signal - out',
+                          ' Road sign or marking defective or obscured',
+                            "Auto signal part defective",
+                             "Data missing or out of range"
+                             )
+conditionchoosen.place(x=40, y=320)
+conditionchoosen.current(0)
+
 
 window.mainloop()
 
